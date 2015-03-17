77e45bff4e5b364744e2d3f673955064d9fe4631
Jacob Francis Powers <xxv9xx@gmail.com>
2015-03-17 17:07:22 -0500

    Experimentation.

--- basic.js
+++ basic.js
@@ -1,4 +1,4 @@
-var services, config;
+var services, config, debug, experiments;
 global.ports = {
   telnet: null,
   httpd: null,
@@ -73,12 +73,14 @@ function $setting(name) {
 (function configure() {
   services = require("./services.js");
 
+  experiments = [];
   config = {flags: []};
 
   Array.prototype.forEach.call(arguments, identify);
 
   ports.telnet = $setting("telnet-port");
   ports.httpd = $setting("httpd-port");
+  debug = !$setting("experiment");
 }).apply(null, process.argv.slice(2));
 
 //Turn some gears...
@@ -92,12 +94,66 @@ function $setting(name) {
 
 //Start the services.
 (function finalize() {
+  if (debug)
+    return experiment();
+
   services.httpd();
 
   /*telnet is disabled for maintenance.
   services.telnet();//*/
 }).call(null);
 
+function prepare(){
+experiments.push(function(){
+  console.log("Running database test: ");
+  var db, ri, table, ur;
+
+  try {
+    db = new database();
+    console.log("Database created successfully.");
+  }
+  catch(e){ throw "Couldn't instantiate database"}
+
+  try{
+    ri = new db.row("username", "password");
+  }catch(e){ throw "Couldn't create row:index"}
+
+  try{
+    table = new db.table(ri);
+    console.log("Database table created sucessfully.");
+  }catch(e){ throw "Couldn't create a new table";}
+
+  try{
+    ur = new db.row("scientist", "theoretical");
+    table.insert(ur);
+    console.log("Sucessful table insert completed.");
+  }catch(e){
+    throw "Inserting row into database not allowed."
+  }
+  try
+  {console.log(table.query("username","scientist"));}
+  catch(e){
+    throw "Something went wrong attempting to query the database."}
+
+  console.log("Benchmarking database access.");
+
+  table = new db.table(new db.row("id", "seed"));
+
+  for (var i = Math.pow(10, 6); i--; table.insert(ur))
+       ur = new db.row(i, Math.floor(Math.random() * i));
+
+});
+}
+function experiment() {
+  prepare();
+  for (var test in experiments) {
+    try {
+      experiments[test].call(null);
+    } catch(e) {
+      console.error(e.toString());
+    }
+  }
+}
 /*
 index.html
 admin.html
